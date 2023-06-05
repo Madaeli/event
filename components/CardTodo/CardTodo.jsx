74922face74f677fa9be840d2a8e44c849eeed6d
Madaeli <myvaneli@gmail.com>
2023-06-05 09:16:09 +0200

    Card

--- components/CardTodo/CardTodo.jsx
+++ components/CardTodo/CardTodo.jsx
@@ -1,11 +1,12 @@
 import { TouchableOpacity } from "react-native";
 import { Text, Image} from "react-native";
 import checkImg from "../../assets/check.png";
+import { s } from "./CardTodo.style";
 export function CardTodo({ todo}){
   return(
-    <TouchableOpacity>
-       <Text> {todo.title}</Text>
-        <Image   source= {checkImg}  />
+    <TouchableOpacity style= { s.card } >
+       <Text style={s.txt}> {todo.title}</Text>
+        <Image  style={s.img} source= {checkImg}  />
     </TouchableOpacity>
   ) ;
 }
